a60037f40548554a1c2e636003ab3da3bbc74a36
attarmau <attarmau111@gmail.com>
2025-04-27 12:47:16 +0800

    Update retriever.py

--- app/retriever.py
+++ app/retriever.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 import streamlit as st
 import tweepy
 
-REDDIT_HEADERS = {"User-Agent": "finfriend-bot"}
+REDDIT_HEADERS = {"User-Agent": "finfriend-bot/1.0 (by u/Imaginary-Weird-7959)"}
 NEWSAPI_KEY = st.secrets.get("NEWS_API_KEY")
 TWITTER_BEARER_TOKEN = st.secrets.get("TWITTER_BEARER_TOKEN")
 
